9c027891411619c0be9e30b3f893ddee512e12dc
Andrew Morgan <andrew.morgan@mongodb.com>
2025-03-14 20:05:53 +0000

    Generate the customer's email address from their first and last name

--- src/document.js
+++ src/document.js
@@ -24,12 +24,15 @@ const sampleFields = [
   { key: "willingToTestify", values: [true, false] }
 ];
 
-// console.log(sampleFields[0].values);
-
 function getValue(key) {
   const field = sampleFields.find(f => f.key === key);
     return field && field.values ? field.values[Math.floor(Math.random() * field.values.length)]: `No values found for ${key}`;
 };
+
+function genEmailFromName(first, last) {
+  const domain = getValue("domains");
+  return `${first}.${last}@${domain}`;
+}
 
 function genDate() {
   return faker.date.past();
@@ -136,6 +139,9 @@ const sampleDoc = {
 };
 
 function genDocument() {
+  const firstName = getValue("firstNames");
+  const lastName = getValue("lastNames");
+  const email = genEmailFromName(firstName, lastName);
   return {
     ...sampleDoc,
     region: genInt(1, 50),
@@ -151,7 +157,7 @@ function genDocument() {
       },
       contact: {
         phone: getValue("phoneNumbers"),
-        email: getValue("emails")
+        email: email
       }
     },
     policy_details: {
